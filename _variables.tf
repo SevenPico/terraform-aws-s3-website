d985132a27a04918d79e4d08ac7e4c847c808983
Jas Tandon <jtandon@sigmasolve.net>
2023-05-12 13:36:17 -0500

    testing. List(any)

--- _variables.tf
+++ _variables.tf
@@ -119,10 +119,7 @@ variable "s3_object_ownership" {
 }
 
 variable "cloudfront_function_association" {
-  type = list(object({
-    event_type   = string
-    function_arn = string
-  }))
+  type = list(any)
   default = []
   description = <<-EOT
   List of objects representing CloudFront function associations.
