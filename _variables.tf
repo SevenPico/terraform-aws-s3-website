32b856e163b3bbf963decfdf37687f5b0a674cd0
Jas Tandon <jtandon@sigmasolve.net>
2023-05-12 12:23:52 -0500

    reverting back to list of object.

--- _variables.tf
+++ _variables.tf
@@ -119,7 +119,7 @@ variable "s3_object_ownership" {
 }
 
 variable "cloudfront_function_association" {
-  type = list(any)
+  type = list(object)
   default = []
   description = <<-EOT
   list(object({
